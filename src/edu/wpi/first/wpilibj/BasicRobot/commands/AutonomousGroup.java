60f713ca22a6111d18bca46756b1a46644a91573
William Stevens <makefan123@yahoo.com>
2014-09-18 09:10:46 -0400

    v0 Camera code added.
    Added camera code from 2014 vision example and added autonomous command
    command DriveWhenHot. Modified to fit command based model. Untested as
    of 9/18/14.

--- src/edu/wpi/first/wpilibj/BasicRobot/commands/AutonomousGroup.java
+++ src/edu/wpi/first/wpilibj/BasicRobot/commands/AutonomousGroup.java
@@ -13,9 +13,6 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousGroup extends CommandGroup {
     
     public AutonomousGroup() {        
-        // Drive forward for 2.5 seconds
-        addSequential(new DriveForTime(2.5, 0.7, true));
-        // Then stop
-        addSequential(new DriveForTime(0.5, 0.0, false));
+        addSequential(new DriveWhenHot());
     }
 }
